d09057340c43aa39e81c49cc0d7f094c9de25976
TT <xwTT@users.noreply.github.com>
2022-07-24 10:20:45 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import '../styles/globals.scss';
 import { Layout } from '../components';
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script strategy='lazyOnload' async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7587426308752562"
+        crossorigin="anonymous"></Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
