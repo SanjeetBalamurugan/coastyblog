c68773ebd31baf9fde646ace464fcf9867680243
TT <xwTT@users.noreply.github.com>
2022-07-24 10:48:30 +0530

    finally

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Script from "next/script";
+import Script from 'next/script';
 import '../styles/globals.scss';
 import { Layout } from '../components';
 
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        strategy='lazyOnload'
+        strategy="lazyOnload"
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7587426308752562"
         crossorigin="anonymous"
